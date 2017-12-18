49606e9109865fa669f8e600aab4119417b2abde
Oleg979 <33599959+Oleg979@users.noreply.github.com>
2017-12-18 23:03:32 +0400

    Update and rename Server/DeAuth.php to Server/Functions/DeAuth.php

--- Server/Functions/DeAuth.php
+++ Server/Functions/DeAuth.php
@@ -1,7 +1,8 @@
+// Функция деавторизации
 <?php
     function DeAuth($LoginClient,$UsersINI){
         $UsersINI->addParam($LoginClient, "EndTime", '');
-        $return = 'Good|DeAuth';
-    return $return;
+        $result = 'Good|DeAuth';
+    return $result;
     }
 ?>
