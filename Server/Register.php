635b4e8c52b7e38d60de42ef8755f5f66c179dd2
Oleg979 <33599959+Oleg979@users.noreply.github.com>
2017-12-18 23:11:44 +0400

    Delete Register.php

--- Server/Register.php
+++
@@ -1,7 +0,0 @@
-<?php
-    function Register($LoginClient,$PasswordClient,$UsersINI){
-        $UsersINI->addParam($LoginClient, "Password", $PasswordClient);
-        $return = 'Good|UserRegister';
-    return $return;
-    }
-?>
