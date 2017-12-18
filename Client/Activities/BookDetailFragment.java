27d2a5f1eca113402ae45590b15e3f0862983ea0
Oleg979 <33599959+Oleg979@users.noreply.github.com>
2017-12-18 23:30:41 +0400

    Update and rename BookDetailFragment.java to Client/Activities/BookDetailFragment.java

---
+++ Client/Activities/BookDetailFragment.java
@@ -0,0 +1,39 @@
+// Вспомогательный класс для вывода информации о книге
+
+package sstu_team.book;
+
+import android.app.Activity;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import sstu_team.book.dummy.DummyContent;
+
+
+public class BookDetailFragment extends Fragment {
+    
+    public static final String ARG_ITEM_ID = "item_id";
+
+    public BookDetailFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments().containsKey(ARG_ITEM_ID)) {
+        
+            Activity activity = this.getActivity();
+            CollapsingToolbarLayout appBarLayout = (CollapsingToolbarLayout) activity.findViewById(R.id.toolbar_layout);
+            if (appBarLayout != null) {
+                appBarLayout.setTitle(Metadata.names[Integer.valueOf(Metadata.currentNum)-1]);
+            }
+        }
+
+    }
+}
+
