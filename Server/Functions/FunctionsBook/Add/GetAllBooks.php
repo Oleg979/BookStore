30b4fb84d234370e3d25a623f7f93e263e376117
Oleg979 <33599959+Oleg979@users.noreply.github.com>
2017-12-18 23:09:21 +0400

    Update and rename Server/GetAllBooks.php to Server/Functions/FunctionsBook/Add/GetAllBooks.php

--- Server/Functions/FunctionsBook/Add/GetAllBooks.php
+++ Server/Functions/FunctionsBook/Add/GetAllBooks.php
@@ -1,19 +1,20 @@
+// Функция получения всех книг всех пользователей
+
 <?php
     function GetAllBooks($LoginClient, $BooksINI){
         
         $books_array = $BooksINI->read();
         $i=0;
         foreach( $books_array as $key => $value){
-            if($key != 'Count' /*and $value['WhoAdded'] == $LoginClient*/) {
+            if($key != 'Count') {
                 $result[$i]['Id'] = $value['Id'];
                 $result[$i]['Name'] = $value['Name'];
                 $result[$i]['WhoAdded'] = $value['WhoAdded'];
                 $i++;
             }
         }
+       
+    return $result; 
         
-        
-        
-    return $result;    
    }
 ?>
