4a0f82adc65b7abf72ab15eded886ab2937cc4d2
Oleg979 <33599959+Oleg979@users.noreply.github.com>
2018-01-06 20:00:40 +0400

    Update Metadata.java

--- Client/Others/Metadata.java
+++ Client/Others/Metadata.java
@@ -2,23 +2,31 @@
 
 package sstu_team.book;
 
-/**
- * Created by gt99 on 17.12.2017.
- */
+import okhttp3.MediaType;
 
 public class Metadata {
+
+    public static final MediaType JSON
+            = MediaType.parse("application/json; charset=utf-8");
+
+    public  static String url = "http://gt99.xyz/Book/Main.php";
 
     public static String login;
     public  static  String password;
 
+    public static String currentResponse;
     public static String currentNum;
     public static String currentId;
     public static String currentEditor;
+    public static String currentTime;
+
     public static int count;
+    public static int type;
 
     public static String names[];
     public static String ids[];
     public  static String editors[];
+    public  static String times[];
 
     public static String EditName;
     public static String EditAuthor;
