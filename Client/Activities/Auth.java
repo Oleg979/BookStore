930a64ddd229dd48641ef685bad5c3105e1ffe5d
Oleg979 <33599959+Oleg979@users.noreply.github.com>
2018-01-06 19:57:54 +0400

    Update Auth.java

--- Client/Activities/Auth.java
+++ Client/Activities/Auth.java
@@ -4,12 +4,17 @@ package sstu_team.book;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.apache.http.client.HttpClient;
@@ -31,15 +36,24 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Auth extends Activity implements View.OnClickListener{
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class Auth extends Activity{
 
     Button reg;
     AutoCompleteTextView login;
     EditText password;
-    public static String result = "Server denied";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
 
@@ -47,72 +61,93 @@ public class Auth extends Activity implements View.OnClickListener{
         login = (AutoCompleteTextView) findViewById(R.id.login);
         password = (EditText) findViewById(R.id.password);
 
-        reg.setOnClickListener(this);
         reg.setText("Войти или зарегистрироваться");
+
     }
 
-    @Override
-    public void onClick(View view) {
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    public void onRegBtnClick(View view) {
 
+        final String log = login.getText().toString();
+        final String pass = password.getText().toString();
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                final String log = login.getText().toString();
-                final String pass = password.getText().toString();
-                HttpClient httpclient = new DefaultHttpClient();
-                HttpPost http = new HttpPost("http://gt99.xyz/Book/Main.php");
-                List nameValuePairs = new ArrayList(2);
-                nameValuePairs.add(new BasicNameValuePair("Function", "Auth"));
-                nameValuePairs.add(new BasicNameValuePair("Login", log));
-                nameValuePairs.add(new BasicNameValuePair("Password", pass));
-                try {
-                    http.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-                } catch (UnsupportedEncodingException e) {
-                    e.printStackTrace();
-                }
-                //получаем ответ от сервера
-                try {
-                    result = (String) httpclient.execute(http, new BasicResponseHandler());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }).start();
-
+        PostRequest example = new PostRequest();
+        RequestBody requestBody = formRequestBody(log,pass);
+        Callback callback = formCallback();
 
         try {
-            Thread.sleep(500);
-        } catch (InterruptedException e) {
+            example.post(Metadata.url, requestBody, callback);
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
+    }
+
+    private RequestBody formRequestBody(String log, String pass) {
+        RequestBody requestBody = new MultipartBody.Builder()
+                .setType(MultipartBody.FORM)
+                .addFormDataPart("Function", "Auth")
+                .addFormDataPart("Login", log)
+                .addFormDataPart("Password", pass)
+                .build();
+        return requestBody;
+    }
+
+    private Callback formCallback() {
+        return new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.isSuccessful()) {
+                    String responseStr = response.body().string();
+                    Log.w("RESPONSE", responseStr);
+                    checkResponse(responseStr);
+                } else {
+                    Log.w("RESPONSE", "No response");
+                }
+
+            }
+        };
+    }
+
+    private void checkResponse(String response) {
+
         JSONObject json;
-        String resp = "Error:You Pidor!";
 
         try{
-             json = new JSONObject(result);
-             resp = json.getString("Result");
+            json = new JSONObject(response);
+            response = json.getString("Result");
+            Log.w("RESULT",response);
         } catch (JSONException e){}
 
-
-        if(resp.equals("Error")) {
-
-            Snackbar.make(findViewById(R.id.reg_button), "Ошибка. Попробуйте ещё раз!", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show();
+        if(response.equals("Error")) {
+            Snackbar snack = Snackbar.make(findViewById(R.id.reg_button),
+                    "Ошибка. Попробуйте ещё раз!", Snackbar.LENGTH_SHORT)
+                    .setAction("Action", null);
+            View snackbarView = snack.getView();
+            TextView snackTextView = (TextView) snackbarView.findViewById(android.support.design.R.id.snackbar_text);
+            snackTextView.setTextColor(Color.WHITE);
+            snack.show();
         }
 
-        else if (resp.equals("Good")) {
+        else if (response.equals("Good")) {
+            Metadata.type = 0;
             Metadata.login = login.getText().toString();
             Metadata.password = password.getText().toString();
             Intent intent = new Intent(Auth.this, BookListActivity.class);
             startActivity(intent);
-            login.setText("");
-            password.setText("");
-        }
 
+        }
     }
+
 }
+
+
+
 
 
